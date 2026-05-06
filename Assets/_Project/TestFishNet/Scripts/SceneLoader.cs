745a5eddadd45bd6493da50f824b9083b3ad7cc6
Razumov Dmitry <47585081+gedenteen@users.noreply.github.com>
2026-05-06 15:19:05 +0500

    TestFishNet: implement immediate loading players to other scene

--- Assets/_Project/TestFishNet/Scripts/SceneLoader.cs
+++ Assets/_Project/TestFishNet/Scripts/SceneLoader.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Linq;
 using FishNet;
+using FishNet.Connection;
 using FishNet.Managing.Scened;
 using FishNet.Object;
 using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    public bool SceneStack = false;
+    public bool SceneStack = true;
+    public Collider MyCollider;
 
     private float _timer;
     private bool _isLoading;
@@ -16,13 +19,19 @@ public class SceneLoader : MonoBehaviour
 
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
+
         InstanceFinder.SceneManager.OnLoadEnd += SceneManager_OnLoadEnd;
+        InstanceFinder.SceneManager.OnClientLoadedStartScenes += SceneManagerOnOnClientLoadedStartScenes;
     }
 
     private void OnDestroy()
     {
         if (InstanceFinder.SceneManager != null)
+        {
             InstanceFinder.SceneManager.OnLoadEnd -= SceneManager_OnLoadEnd;
+            InstanceFinder.SceneManager.OnClientLoadedStartScenes -= SceneManagerOnOnClientLoadedStartScenes;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,6 +39,17 @@ public class SceneLoader : MonoBehaviour
         NetworkObject networkObject = other.GetComponent<NetworkObject>();
         if (networkObject != null)
             LoadScene(networkObject);
+    }
+
+    private void LoadScene(NetworkConnection networkConnection)
+    {
+        SceneLookupData lookup = new SceneLookupData(_stackedSceneHandle, _sceneName);
+        SceneLoadData sld = new SceneLoadData(lookup);
+        sld.Options.AllowStacking = true;
+
+        sld.MovedNetworkObjects = networkConnection.Objects.ToArray();
+        sld.ReplaceScenes = ReplaceOption.All;
+        InstanceFinder.SceneManager.LoadConnectionScenes(networkConnection, sld);
     }
 
     private void LoadScene(NetworkObject networkObject)
@@ -41,22 +61,28 @@ public class SceneLoader : MonoBehaviour
         sld.MovedNetworkObjects = new NetworkObject[] { networkObject };
         sld.ReplaceScenes = ReplaceOption.All;
         InstanceFinder.SceneManager.LoadConnectionScenes(networkObject.Owner, sld);
-        // InstanceFinder.SceneManager.LoadGlobalScenes(sld);
+    }
+
+    private void SceneManagerOnOnClientLoadedStartScenes(NetworkConnection arg1, bool arg2)
+    {
+        LoadScene(arg1);
     }
 
     private void SceneManager_OnLoadEnd(SceneLoadEndEventArgs obj)
     {
-        //
+        // Server handles scene loading and syncing
+        // so do not bother setting up scene stacking if it was a client
+        // that completed the scene load
         if (!obj.QueueData.AsServer)
             return;
         if (!SceneStack)
             return;
 
-        //
+        // Stacked scene id is already set, not interested in creating a new stacked scene
         if (_stackedSceneHandle != 0)
             return;
 
-        //
+        // Set the first loaded scene as the handle
         if (obj.LoadedScenes.Length > 0)
             _stackedSceneHandle = obj.LoadedScenes[0].handle;
     }
