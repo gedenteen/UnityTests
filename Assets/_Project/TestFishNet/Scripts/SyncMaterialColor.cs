e1d5af82b92ca577824da0dcbc223eaa59cc7870
Razumov Dmitry <47585081+gedenteen@users.noreply.github.com>
2026-05-13 11:38:31 +0500

    TestFishNet: implement changing color by pressing Space

--- Assets/_Project/TestFishNet/Scripts/SyncMaterialColor.cs
+++ Assets/_Project/TestFishNet/Scripts/SyncMaterialColor.cs
@@ -2,6 +2,8 @@ using System;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = System.Random;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class SyncMaterialColor : NetworkBehaviour
@@ -17,6 +19,24 @@ public class SyncMaterialColor : NetworkBehaviour
     private void OnDestroy()
     {
         Color.OnChange -= OnColorChanged;
+    }
+
+    private void Update()
+    {
+        if (!IsOwner)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeToRandomColor();
+        }
+    }
+
+    [ServerRpc]
+    private void ChangeToRandomColor()
+    {
+        Color.Value = UnityEngine.Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.5f, 1f);
+        Debug.Log($"SyncMaterialColor: ChangeToRandomColor: {Color.Value}");
     }
 
     private void OnColorChanged(Color previous, Color next, bool asServer)
